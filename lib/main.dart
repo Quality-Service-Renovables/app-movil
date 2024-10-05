1c25171ee61d02dbbc1418cd9b9e20ecac0a907f
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-10-05 16:13:53 -0600

    App finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'QSR Eolic Inspection test',
+      title: 'QSR Eolic Inspection',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
